b60b681fc1b9fe6e7dc056e47b699b7c43eb3f3c
tg-who <werner.hoelbling@technikgruppe.com>
2025-06-11 03:24:14 +0200

    Refactor of project structure

---
+++ src/fls_cli/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fls-cli!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/fls_svc/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fls-svc!")
+
+
+if __name__ == "__main__":
+    main()
